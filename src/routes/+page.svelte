192ac933e6d89ce3aff80b4b84b860e77cf62f1d
amal <amal_266@icloud.com>
2024-07-22 23:31:52 +0300

    red time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<div class=" p-16">
+	<header class=" flex justify-between">
+		<div>
+			<h4 class=" text-xl text-yellow-950 mb-2">
+				صباح الخير👋
+					
+			</h4>
+			<h2 class=" text-xs text-surface-900/50 ">
+				اليوم ،السبت 14\محرم 1446
+			</h2>
+		</div>
+		
+		<div class=" bg-pink-600 w-24 h-24">
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+		</div>
+	</header>
 </div>
+
