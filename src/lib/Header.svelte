eb9f6e78efdc9f19d653f2b55abbb82df6119fb5
amal <amal_266@icloud.com>
2024-08-02 07:00:45 +0300

    v

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -38,7 +38,7 @@ const popupCombobox: PopupSettings = {
         </div>
         
         
-<button class="btn variant-filled w-15  sm:w-15  px-2 justify-between" use:popup={popupCombobox}>
+<button class="btn variant-filled  w-13    px-2 justify-between" use:popup={popupCombobox}>
 	<span >{$filter }</span>
 	<span class=" !m-0"><svg id="arrow"
           xmlns="http://www.w3.org/2000/svg" class=" w-6 rotate-180 h-fit py-1 transition-all" viewBox="0 0 24 24"><path fill="currentColor" d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82"/></svg></span>
